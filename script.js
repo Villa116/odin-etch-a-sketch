cf598453e2a4cfb337bf8a5b9d78325ebbecb51a
Villa116 <66364019+Villa116@users.noreply.github.com>
2026-03-20 22:14:14 +0200

    Add hover effect to grid to change square color
    Each square changes to a random color when the mouse passes over it.
    Each mouse interaction darkens the square by 10%.
    The square becomes fully black after 10 mouse interactions.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*total space of the container div with same size for width and height (700px)*/
-const containerSize = 700;
+
 const initialSquareDivSize = 16;
 
 const divContainer = document.querySelector(".container");
@@ -20,8 +20,29 @@ function fillUpContainerDiv(numberOfDivs) {
 
     squareDiv.style.flex = `1 1 auto`;
     squareDiv.style.boxSizing = "border-box";
-    squareDiv.style.border = "1px solid black";
+    squareDiv.style.border = "1px solid rgba(0, 0, 0, 0.05)";
+
+    squareDiv.dataset.hue = Math.floor(Math.random() * 360);
+    squareDiv.dataset.lightness = 100;
 
     divContainer.appendChild(squareDiv);
   }
 }
+
+
+divContainer.addEventListener("mouseover", (e) => {
+  const target = e.target;
+
+  if (e.target !== divContainer) {
+    let squareDivLightness = parseFloat(target.dataset.lightness);
+    const squareDivHue = target.dataset.hue;
+
+    if (squareDivLightness > 0) {
+      squareDivLightness -= 10;
+      target.dataset.lightness = squareDivLightness;
+
+      target.style.backgroundColor = `hsl(${squareDivHue}, 100%, ${squareDivLightness}%)`;
+      target.style.border = "none";
+    }
+  }
+});
